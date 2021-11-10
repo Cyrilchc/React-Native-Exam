f0076d95ee52a4341ea9886d2c4f77e80473b58c
Cyril <cyrilchallouatte54@gmail.com>
2021-11-10 17:25:11 +0100

    Ajout des fichiers manquants

---
+++ Shop/Components/Products.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Products = ({ navigation }) => {
+    return (
+        <View>
+            <Text>Products</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+});
+
+export default Products;
